58a324c1c05e027a2f0bf588ad03a2bbe8e88dd3
tlaranje <tlaranje@student.42.porto.com>
2026-07-30 15:48:38 +0100

    Add files

---
+++ srcs/requirements/nginx/tools/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ ! -f /etc/ssl/certs/nginx-selfsigned.crt ]; then
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+        -keyout /etc/ssl/private/nginx-selfsigned.key \
+        -out /etc/ssl/certs/nginx-selfsigned.crt \
+        -subj "/C=PT/ST=Porto/L=Porto/O=42/OU=Inception/CN=localhost"
+fi
+
+exec nginx -g "daemon off;"
